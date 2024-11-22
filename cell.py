8a0730cb41a6424ffe6404f6b7aac596de564de4
aahm2221 <aisha.ahmed1@yahoo.com>
2024-11-22 17:48:53 -0600

    add draw move

--- cell.py
+++ cell.py
@@ -27,6 +27,8 @@ class Cell():
         if self.has_bottom_wall:
             self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)), "black")
 
-
-
-   
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "gray"
+        self._win.draw_line(Line(Point((self._x2+self._x1)/2, (self._y1+self._y2)/2), Point((to_cell._x2+to_cell._x1)/2, (to_cell._y1+to_cell._y2)/2)), color)
